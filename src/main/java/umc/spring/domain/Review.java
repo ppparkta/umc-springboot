0017edf891fb9669ad5c11d4e4458ce4dd8ccaf0
ppparkta <pppppark2@gmail.com>
2023-11-08 16:19:01 +0900

    feat: entity 개발

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,22 @@
+package umc.spring.domain;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+    private String title;
+    private Float score;
+}
